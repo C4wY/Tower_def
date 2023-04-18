2bf28679a53531f2677ed25465ad2d0d3998a9fa
jniac <joseph.merdrignac@gmail.com>
2023-04-18 17:59:40 +0200

    onLook

--- Assets/Scripts/CameraInputAction.cs
+++ Assets/Scripts/CameraInputAction.cs
@@ -5,6 +5,11 @@ using UnityEngine.InputSystem;
 
 public class CameraInputAction : MonoBehaviour
 {
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnLook {context.ReadValue<Vector2>()}");
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         Debug.Log($"OnMove {context.ReadValue<Vector2>()}");
